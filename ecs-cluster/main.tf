0ec5171bed650ca1fedbfa6fd6738de67a0a547f
Bryan <bryan00iwe@gmail.com>
2025-09-04 01:26:08 +0800

    ecs cluster new naming convention

--- ecs-cluster/main.tf
+++ ecs-cluster/main.tf
@@ -2,27 +2,31 @@ terraform {
   backend "s3" {}
 }
 
-variable name {
+variable cluster_name {
+    type = string
+}
+
+variable project_name {
     type = string
 }
 
 # ==================================================================================
 resource "aws_kms_key" "kms-key" {
-  description = "Key for ECS cluster"
+  description = "Key for ecs-cluster-${var.project_name}-${var.cluster_name}"
 }
 
 resource "aws_kms_alias" "kms-alias" {
-  name          = "alias/kms-key-${var.name}"
+  name          = "alias/kms-key-${var.project_name}-${var.cluster_name}"
   target_key_id = aws_kms_key.kms-key.key_id
 }
 
 resource "aws_cloudwatch_log_group" "loggroup" {
-  name = "cwlogs-${var.name}"
+  name = "cwloggrp-${var.project_name}-${var.cluster_name}"
   retention_in_days = 90
 }
 
 resource "aws_ecs_cluster" "ecs-cluster" {
-  name = "ecs-cluster-${var.name}"
+  name = "ecs-cluster-${var.project_name}-${var.cluster_name}"
 
   configuration {
     execute_command_configuration {
@@ -30,7 +34,7 @@ resource "aws_ecs_cluster" "ecs-cluster" {
       logging = "OVERRIDE"
       log_configuration {
         cloud_watch_encryption_enabled = true
-        cloud_watch_log_group_name = "cwlogs-${var.name}"
+        cloud_watch_log_group_name = "cwloggrp-${var.project_name}-${var.cluster_name}"
       }
     }
   }
