7308e99c14934110118cc11e206ca27d089938e4
Bryan <bryan00iwe@gmail.com>
2025-09-03 22:02:44 +0800

    s3 module dynamic acct number, new naming convention

--- s3/main.tf
+++ s3/main.tf
@@ -2,14 +2,22 @@ terraform {
   backend "s3" {}
 }
 
-variable name {
+variable account_no {
+  type = string
+}
+
+variable project_name {
+  type = string
+}
+
+variable bucket_name {
   type = string
 }
 
 # ==================================================================================
 
 resource "aws_s3_bucket" "s3" {
-  bucket = var.name
+  bucket = "storage-s3-${var.project_name}-${var.bucket_name}"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse" {
@@ -31,10 +39,10 @@ resource "aws_s3_bucket_policy" "bucket_owner_full_control" {
       {
         "Effect": "Allow",
         "Principal": {
-          "AWS": "arn:aws:iam::[aws_account_no]:root"
+          "AWS": "arn:aws:iam::${var.account_no}:root"
         },
         "Action": "s3:PutObject",
-        "Resource": "[s3_bucket_arn]/*"
+        "Resource": "${aws_s3_bucket.s3.arn}/*"
       }
     ]
   })
